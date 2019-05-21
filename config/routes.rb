9fb264345cc95ba239b46c07db1d5f7a31551595
野崎陽奈子 <nozakihinako@nozakihinakonoMacBook-Pro.local>
2019-05-21 19:03:15 +0900

    ログイン完成

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-  get 'eateries/new'
-  post 'eateries/create'
-    get 'lunch/go'
-    root 'lunch#go'
-    get '/login', to:'sessions#new'
-    post 'sessions/create'
+ get 'eateries/new'
+ post 'eateries/create'
+ get 'lunch/go'
+ root 'lunch#go'
+ get '/login', to:'sessions#new'
+ post 'sessions/create'
 end
